0996fbd9548dbc04036247dc828e7d6c581be9d1
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-16 22:02:50 +0800

    add profile picture

--- app/src/main/java/sg/edu/np/mad/freeflow/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/MainActivity.java
@@ -3,17 +3,26 @@ package sg.edu.np.mad.freeflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser user;
+
     TextView usernameTextView;
+    ImageView profileImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         usernameTextView = findViewById(R.id.username_text_view);
+        profileImageView = findViewById(R.id.profile_image_view);
     }
 
     @Override
@@ -35,8 +45,32 @@ public class MainActivity extends AppCompatActivity {
             startActivity(signInActivity);
         } else {
             user = mAuth.getCurrentUser();
-
-            usernameTextView.setText("Hello " + user.getDisplayName() + "!");
+            setUpUser();
         }
+    }
+
+    private void setUpUser() {
+        usernameTextView.setText("Hello " + user.getDisplayName() + "!");
+
+        // Add profile picture
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(user.getPhotoUrl().toString());
+
+                    Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+
+                    runOnUiThread(()->{
+                        profileImageView.setImageBitmap(bmp);
+                    });
+                    System.out.println("success");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
