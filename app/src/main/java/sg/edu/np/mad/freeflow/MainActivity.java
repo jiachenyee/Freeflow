df20b93f8f78414cd414b265fe06039d3a4fdf7c
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-16 15:08:39 +0800

    create sign in activity, display username

--- app/src/main/java/sg/edu/np/mad/freeflow/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/MainActivity.java
@@ -2,13 +2,41 @@ package sg.edu.np.mad.freeflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    TextView usernameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        usernameTextView = findViewById(R.id.username_text_view);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // If the user is not signed in, get the user to sign in
+        if (mAuth.getCurrentUser() == null) {
+            Intent signInActivity = new Intent(MainActivity.this, SignInActivity.class);
+
+            startActivity(signInActivity);
+        } else {
+            user = mAuth.getCurrentUser();
+
+            usernameTextView.setText("Hello " + user.getDisplayName() + "!");
+        }
     }
 }
