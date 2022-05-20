184880bf12d1c49a2e3c418c2bc5b5676c00045b
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-21 01:53:18 +0800

    automatically present keyboard in new workspace

--- app/src/main/java/sg/edu/np/mad/freeflow/NewWorkspaceActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/NewWorkspaceActivity.java
@@ -2,7 +2,13 @@ package sg.edu.np.mad.freeflow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class NewWorkspaceActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class NewWorkspaceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_workspace);
+
+        ImageButton closeButton = findViewById(R.id.close_button);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EditText workspaceNameEditText = findViewById(R.id.workspace_name_edit_text);
+
+        workspaceNameEditText.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                workspaceNameEditText.setFocusableInTouchMode(true);
+                workspaceNameEditText.requestFocus();
+                getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(workspaceNameEditText, 0);
+            }
+        }, 0);
     }
 }
