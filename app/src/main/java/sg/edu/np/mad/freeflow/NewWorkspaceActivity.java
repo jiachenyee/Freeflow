73135a92b5bf21714f05fbd832fbc5d401021fad
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-21 22:46:48 +0800

    implement accent color selector

--- app/src/main/java/sg/edu/np/mad/freeflow/NewWorkspaceActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/NewWorkspaceActivity.java
@@ -7,10 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class NewWorkspaceActivity extends AppCompatActivity {
+
+    private MaterialCardView workspaceAccent1;
+    private MaterialCardView workspaceAccent2;
+    private MaterialCardView workspaceAccent3;
+    private MaterialCardView workspaceAccent4;
+    private MaterialCardView workspaceAccent5;
+    private MaterialCardView workspaceAccent6;
+
+    private int selectedColorIndex = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +39,8 @@ public class NewWorkspaceActivity extends AppCompatActivity {
         });
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+
+        setUpAccentColorSelector();
     }
 
     @Override
@@ -43,5 +57,72 @@ public class NewWorkspaceActivity extends AppCompatActivity {
                 ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(workspaceNameEditText, 0);
             }
         }, 0);
+    }
+
+    private void setUpAccentColorSelector() {
+        workspaceAccent1 = findViewById(R.id.workspace_accent_1);
+        workspaceAccent1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColorIndex = 1;
+                updateAccentColorSelector();
+            }
+        });
+
+        workspaceAccent2 = findViewById(R.id.workspace_accent_2);
+        workspaceAccent2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColorIndex = 2;
+                updateAccentColorSelector();
+            }
+        });
+
+        workspaceAccent3 = findViewById(R.id.workspace_accent_3);
+        workspaceAccent3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColorIndex = 3;
+                updateAccentColorSelector();
+            }
+        });
+
+        workspaceAccent4 = findViewById(R.id.workspace_accent_4);
+        workspaceAccent4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColorIndex = 4;
+                updateAccentColorSelector();
+            }
+        });
+
+        workspaceAccent5 = findViewById(R.id.workspace_accent_5);
+        workspaceAccent5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColorIndex = 5;
+                updateAccentColorSelector();
+            }
+        });
+
+        workspaceAccent6 = findViewById(R.id.workspace_accent_6);
+        workspaceAccent6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColorIndex = 6;
+                updateAccentColorSelector();
+            }
+        });
+
+        updateAccentColorSelector();
+    }
+
+    private void updateAccentColorSelector() {
+        workspaceAccent1.setStrokeWidth(selectedColorIndex == 1 ? 21 : 0);
+        workspaceAccent2.setStrokeWidth(selectedColorIndex == 2 ? 21 : 0);
+        workspaceAccent3.setStrokeWidth(selectedColorIndex == 3 ? 21 : 0);
+        workspaceAccent4.setStrokeWidth(selectedColorIndex == 4 ? 21 : 0);
+        workspaceAccent5.setStrokeWidth(selectedColorIndex == 5 ? 21 : 0);
+        workspaceAccent6.setStrokeWidth(selectedColorIndex == 6 ? 21 : 0);
     }
 }
