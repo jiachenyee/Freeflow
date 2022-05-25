f7ba0f1313610be7be457091ccc757711ebe964f
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-25 12:21:04 +0800

    initial settings activity setup

--- app/src/main/java/sg/edu/np/mad/freeflow/WorkspaceSettingsActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/WorkspaceSettingsActivity.java
@@ -3,6 +3,7 @@ package sg.edu.np.mad.freeflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class WorkspaceSettingsActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class WorkspaceSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workspace_setting);
+
+        findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
