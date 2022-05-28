1a8d5f3fa8e60ddee2446af99db5546fb639443b
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-28 18:21:34 +0800

    minor styling fixes on invite button; add header accent on settings

--- app/src/main/java/sg/edu/np/mad/freeflow/WorkspaceSettingsActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/WorkspaceSettingsActivity.java
@@ -1,9 +1,11 @@
 package sg.edu.np.mad.freeflow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class WorkspaceSettingsActivity extends AppCompatActivity {
 
@@ -25,5 +27,7 @@ public class WorkspaceSettingsActivity extends AppCompatActivity {
     private void setUp() {
         Bundle extras = this.getIntent().getExtras();
 
+        LinearLayout workspaceActivityHeader = findViewById(R.id.header_view);
+        workspaceActivityHeader.setBackgroundResource(Workspace.colors[extras.getInt("workspaceAccentColor",0)]);
     }
 }
