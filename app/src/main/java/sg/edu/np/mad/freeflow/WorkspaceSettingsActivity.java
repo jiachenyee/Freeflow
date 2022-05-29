9a06a57bc6135ed3a979ab557964ecd56be5ab45
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-05-29 21:52:00 +0800

    update error fragment to reflect Figma design

--- app/src/main/java/sg/edu/np/mad/freeflow/WorkspaceSettingsActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/WorkspaceSettingsActivity.java
@@ -5,6 +5,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,6 +48,11 @@ public class WorkspaceSettingsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent workspaceInviteActivity = new Intent(WorkspaceSettingsActivity.this, WorkspaceInviteActivity.class);
+
+                workspaceInviteActivity.putExtra("workspaceIcon", (Bitmap) extras.getParcelable("workspaceIcon"));
+                workspaceInviteActivity.putExtra("workspaceAccentColor", extras.getInt("workspaceAccentColor"));
+                workspaceInviteActivity.putExtra("workspaceName", extras.getString("workspaceName"));
+                workspaceInviteActivity.putExtra("workspaceInviteCode", extras.getString("workspaceInviteCode"));
 
                 startActivity(workspaceInviteActivity);
             }
