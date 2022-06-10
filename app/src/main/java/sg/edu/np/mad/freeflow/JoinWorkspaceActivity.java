116058a12de6b2539c49df7dbe51c78bc40325f5
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-06-10 14:37:40 +0800

    join workspace dismiss button works, minor interface tweaks

--- app/src/main/java/sg/edu/np/mad/freeflow/JoinWorkspaceActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/JoinWorkspaceActivity.java
@@ -3,12 +3,26 @@ package sg.edu.np.mad.freeflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class JoinWorkspaceActivity extends AppCompatActivity {
+
+    ImageButton closeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_workspace);
+
+        closeButton = findViewById(R.id.close_button);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
