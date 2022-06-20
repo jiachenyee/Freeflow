143923b956da4f99270c5d67383dcae07a282fbe
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-06-20 22:19:23 +0800

    set up new task view xml

--- app/src/main/java/sg/edu/np/mad/freeflow/NewTaskActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/NewTaskActivity.java
@@ -3,12 +3,32 @@ package sg.edu.np.mad.freeflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class NewTaskActivity extends AppCompatActivity {
+
+    Spinner categorySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+
+        categorySpinner = findViewById(R.id.category_spinner);
+
+        ArrayList<String> categories = new ArrayList<>();
+
+        categories.add("Hello");
+        categories.add("Potato");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        adapter.setDropDownViewResource(android.R.layout
+                .simple_spinner_dropdown_item);
+
+        categorySpinner.setAdapter(adapter);
     }
 }
