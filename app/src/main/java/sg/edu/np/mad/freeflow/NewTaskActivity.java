8baef148591e1732a117bd8948d32fad1cf1cfd0
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-06-21 01:25:47 +0800

    add workspace accent color to new task view

--- app/src/main/java/sg/edu/np/mad/freeflow/NewTaskActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/NewTaskActivity.java
@@ -1,9 +1,12 @@
 package sg.edu.np.mad.freeflow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -11,6 +14,7 @@ import java.util.ArrayList;
 public class NewTaskActivity extends AppCompatActivity {
 
     Spinner categorySpinner;
+    Button createButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,7 @@ public class NewTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_task);
 
         categorySpinner = findViewById(R.id.category_spinner);
-
-//        newTaskActivity.putExtra("workspaceAccentColor", extras.getInt("workspaceAccentColor"));
-//        newTaskActivity.putExtra("workspaceID", extras.getString("workspaceID"));
-//        newTaskActivity.putExtra("workspaceCategories", workspace.categories);
+        createButton = findViewById(R.id.create_button);
 
         Bundle extras = getIntent().getExtras();
         ArrayList<String> categories = extras.getStringArrayList("workspaceCategories");
@@ -32,5 +33,25 @@ public class NewTaskActivity extends AppCompatActivity {
                 .simple_spinner_dropdown_item);
 
         categorySpinner.setAdapter(adapter);
+
+        setUpAccentColor(Workspace.colors[extras.getInt("workspaceAccentColor",0)]);
+    }
+
+    private void setUpAccentColor(int colorResource) {
+
+        LinearLayout headerView = findViewById(R.id.header_view);
+
+        headerView.setBackgroundResource(colorResource);
+
+        CardView taskTitleCard = findViewById(R.id.task_title_card);
+        taskTitleCard.setCardBackgroundColor(getResources().getColor(colorResource));
+
+        CardView taskDescriptionCard = findViewById(R.id.task_description_card);
+        taskDescriptionCard.setCardBackgroundColor(getResources().getColor(colorResource));
+
+        CardView taskCategoryCard = findViewById(R.id.task_category_card);
+        taskCategoryCard.setCardBackgroundColor(getResources().getColor(colorResource));
+
+        createButton.setBackgroundResource(colorResource);
     }
 }
