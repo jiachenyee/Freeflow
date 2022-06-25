3d6779782081d6c291227a279fb01eaedf084267
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-06-25 20:08:28 +0800

    initial setup for task recycler view

---
+++ app/src/main/java/sg/edu/np/mad/freeflow/TaskViewHolder.java
@@ -0,0 +1,13 @@
+package sg.edu.np.mad.freeflow;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TaskViewHolder extends RecyclerView.ViewHolder {
+
+    public TaskViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
