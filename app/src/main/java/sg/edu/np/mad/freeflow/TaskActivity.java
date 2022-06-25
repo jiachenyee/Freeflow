45096cfc0f18faf82073a70fe076cf8327051e8b
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-06-25 08:09:36 +0800

    implement task detail view

--- app/src/main/java/sg/edu/np/mad/freeflow/TaskActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/TaskActivity.java
@@ -1,11 +1,27 @@
 package sg.edu.np.mad.freeflow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class TaskActivity extends AppCompatActivity {
+
+    TextView taskTitleTextView;
+    TextView taskDescriptionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +34,34 @@ public class TaskActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        taskTitleTextView = findViewById(R.id.task_title_text_view);
+        taskDescriptionTextView = findViewById(R.id.task_description_text_view);
+
+        Bundle extras = this.getIntent().getExtras();
+        loadFromFirestore(extras);
+        setUpAccentColor(extras);
+    }
+
+    private void loadFromFirestore(Bundle extras) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        String workspaceID = extras.getString("workspaceID");
+        String taskID = extras.getString("taskID");
+
+        DocumentReference docRef = db.collection("workspaces").document(workspaceID).collection("tasks").document(taskID);
+
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                taskTitleTextView.setText((String) documentSnapshot.get("title"));
+                taskDescriptionTextView.setText((String) documentSnapshot.get("description"));
+            }
+        });
+    }
+
+    private void setUpAccentColor(Bundle extras) {
+        int color = extras.getInt("accentColor");
+        findViewById(R.id.task_header).setBackgroundColor(color);
     }
 }
--- app/src/main/java/sg/edu/np/mad/freeflow/WorkspaceTasksAdapter.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/WorkspaceTasksAdapter.java
@@ -51,6 +51,10 @@ public class WorkspaceTasksAdapter extends RecyclerView.Adapter<WorkspaceTasksVi
                 public void onTaskOpenHandler(String taskID) {
                     Intent workspaceTaskActivity = new Intent(activity, TaskActivity.class);
 
+                    workspaceTaskActivity.putExtra("workspaceID", activity.extras.getString("workspaceID"));
+                    workspaceTaskActivity.putExtra("taskID", taskID);
+                    workspaceTaskActivity.putExtra("accentColor", activity.getResources().getColor(Workspace.colors[workspace.accentColor]));
+
                     activity.startActivity(workspaceTaskActivity);
                 }
             });
