02f91113b82423c2d00724249e9fd89d335a90e2
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-07-25 22:29:30 +0800

    create new link activity

---
+++ app/src/main/java/sg/edu/np/mad/freeflow/NewLinkActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad.freeflow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewLinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_link);
+    }
+}
