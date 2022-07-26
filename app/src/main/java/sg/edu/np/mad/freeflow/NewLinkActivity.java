8a6c258c5cd5a79bc40b5152234000c80e39268b
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-07-26 08:47:18 +0800

    add dismiss button to links

--- app/src/main/java/sg/edu/np/mad/freeflow/NewLinkActivity.java
+++ app/src/main/java/sg/edu/np/mad/freeflow/NewLinkActivity.java
@@ -3,6 +3,7 @@ package sg.edu.np.mad.freeflow;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 public class NewLinkActivity extends AppCompatActivity {
@@ -14,11 +15,24 @@ public class NewLinkActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
 
+        setupAccentColor(extras);
+        setUpDismiss();
+    }
+
+    private void setupAccentColor(Bundle extras) {
         int color = extras.getInt("workspaceAccentColor");
 
         LinearLayout headerView = findViewById(R.id.header_view);
 
         headerView.setBackgroundColor(color);
+    }
 
+    private void setUpDismiss() {
+        findViewById(R.id.close_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
